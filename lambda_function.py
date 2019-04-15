675667b8398614a538734f4bfc0a1dad3e6316f5
nestim <32952745+nest1m4107@users.noreply.github.com>
2019-04-15 13:43:40 +0300

    Rewrite main methods

--- lambda_function.py
+++ lambda_function.py
@@ -7,23 +7,47 @@ You need to create new webhook in your chat,
 and replace GOOGLE_WEBHOOK_URL by yours URL
 """
 
-GOOGLE_WEBHOOK_URL = 'https://chat.googleapis.com/v1/spaces/########/messages?key=####################################&token=########################################'
+google_chat_url = 'https://chat.googleapis.com/v1/spaces/########/messages?key=####################################&token=########################################'
 
 
 def lambda_handler(event, context):
-    
-    logging.info("Event: " + str(event))
-    
-    for record in event["Records"]:
-        if type(record["Sns"]["Message"]) == str:
-            contents = json.loads(record["Sns"]["Message"])
-        else:
-            contents = record["Sns"]["Message"]
-        
-        message = "*" + contents["AlarmName"] + ":* " + contents["OldStateValue"] + " ⟶ " + contents["NewStateValue"] + "\n```" + contents["NewStateReason"] +"```"
-        logging.info("Message to GoogleChat: " + str(message))
-        
-        bot_message = {'text': message}
-        message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
-        
-        response = requests.post(url=GOOGLE_WEBHOOK_URL, headers=message_headers, data=json.dumps(bot_message))
+    # print("Received event: " + json.dumps(event, indent=2))
+
+    try:
+        text = get_text(get_message_from_event(event))
+        send_to_chat(text, google_chat_url)
+        return {
+            'statusCode': 200,
+            'body': str(text)
+        }
+
+    except Exception as e:
+        logging.error(f'Exception:{e}. Message{event}')
+        return {
+            'statusCode': 500,
+            'body': 'Send ERROR to chat - ' + str(send_to_chat(f'Something went wrong with sns-to-googlechat:\n```{e}\n{event}```', google_chat_url))
+        }
+
+
+def get_message_from_event(event: dict) -> dict:
+    return json.loads(event.get('Records')[0].get('Sns').get('Message'))
+
+
+def get_text(message: dict) -> str:
+    alarm_name = message['AlarmName'] if 'AlarmName' in message.keys() else ''
+    old_state = message['OldStateValue'] if 'OldStateValue' in message.keys() else ''
+    new_state = message['NewStateValue'] if 'NewStateValue' in message.keys() else ''
+    new_state_reason = message['NewStateReason'] if 'NewStateReason' in message.keys() else ''
+
+    return f'*{alarm_name}:* {old_state} ⟶ {new_state}\n```{new_state_reason}```'
+
+
+def send_to_chat(text: str, webhook_url: str):
+    message = {'text': text}
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    response = requests.post(url=webhook_url, headers=headers, data=json.dumps(message))
+
+    if response.status_code != 200:
+        return False
+
+    return True
